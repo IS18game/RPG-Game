09f78334294113e5f16227cbdfd6e6c6cc9677af
IS18game <61816406+IS18game@users.noreply.github.com>
2024-12-22 20:55:34 +0500

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import Dwarf from './Dwarf';
+import Crossbowman from './Crossbowman';
+import Demourge from './Demourge';
+import Warrior from './Warrior';
+import Archer from './Archer';
+import Mage from './Mage';
+import play from './game';
+
+const players = [
+  new Warrior(0, 'Воин 1'),
+  new Archer(2, 'Лучник 1'),
+  new Mage(4, 'Маг 1'),
+  new Dwarf(6, 'Гном 1'),
+  new Crossbowman(8, 'Арбалетчик 1'),
+  new Demourge(10, 'Демиург 1')
+];
+
+play(players);
+
